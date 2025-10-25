e7fb1f7fca66d0cb25e828e513a61e9d9d57eb2a
Ézhor Malkávar <ezhormalkavar@gmail.com>
2025-10-25 15:00:39 +0200

    Enemy sync

--- Scripts/Enemy.gd
+++ Scripts/Enemy.gd
@@ -3,4 +3,6 @@ extends AbstractPlayer
 
 func _on_prefixed_data(data: String):
 	if int(data.split("@")[0]) == identity:
-		position = str_to_var(data.split("@")[3])
+		var serialized_vector = data.split("@")[2]
+		var new_position: Vector2 = str_to_var("Vector2"+serialized_vector)
+		position = new_position
